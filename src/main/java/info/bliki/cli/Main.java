b5af7541ba1a920da1dae9a6a6c588790fb37581
Chunliang Lyu <hi@chunlianglyu.com>
2015-09-15 10:42:28 +0800

    Handle cases where page text may be null

--- src/main/java/info/bliki/cli/Main.java
+++ src/main/java/info/bliki/cli/Main.java
@@ -9,10 +9,16 @@ import org.xml.sax.SAXException;
 import java.io.*;
 
 /**
+ * convert to tsv in the format of
+ *
+ *   pageId\tw:Title\tRedirect\tText
+ *
+ * Note that Redirect and Text may be `null`
  */
 public class Main {
 
     public static void main(String[] args) throws IOException, SAXException {
+
         if (args.length < 2) {
             System.err.println("Usage: Parser <xml-file.[bz2|gz]> <output>");
             System.exit(-1);
@@ -23,19 +29,30 @@ public class Main {
 
         final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outputFilePath), "UTF-8"));
         new WikiXMLParser(new File(dumpFilePath), new IArticleFilter() {
+
+            private int count = 0;
+
             @Override
             public void process(WikiArticle page, Siteinfo siteinfo) throws SAXException {
 
-                System.out.println(page.getTitle());
+                count += 1;
+                if (count % 10000 == 0) {
+                    System.out.println(count);
+                }
 
                 String title = "w:" + page.getTitle().replace(" ", "_");
-                String redirect;
-                if (page.getRedirect() != null) {
+                String redirect = page.getRedirect();
+                if (redirect != null) {
                     redirect = "w:" + page.getRedirect().replace(" ", "_");
                 } else {
                     redirect = "null";
                 }
-                String text = page.getText().replace("\\", "\\\\").replace("\n", "\\\\n").replace("\t", "\\\\t");
+                String text = page.getText();
+                if (text != null) {
+                    text = text.replace("\\", "\\\\").replace("\n", "\\\\n").replace("\t", "\\\\t");
+                } else {
+                    text = "null";
+                }
 
                 StringBuilder sb = new StringBuilder();
                 sb.append(page.getId()).append('\t')
